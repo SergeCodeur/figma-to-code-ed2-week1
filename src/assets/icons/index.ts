5eabea6eedb8aaf495dcfdb87971ef0920dbf93a
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-02 01:49:25 +0100

    feat : création de l'index pour faciliter les imports des icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Bars } from "./bars";
+export { default as Bitcoin } from "./bitcoin";
+export { default as Close } from "./close";
+export { default as CloudDownload } from "./cloud-download";
+export { default as Ethereum } from "./ethereum";
+export { default as MoveRight } from "./move-right";
+export { default as SaleTag } from "./sale-tag";
+export { default as Wallet } from "./wallet";
