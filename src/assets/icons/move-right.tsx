8f73ddde9a324c8becffcaee96cef5179587537c
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-02 17:37:31 +0100

    feat : ajout de la props className pour ajouter du style à l'icône

--- src/assets/icons/move-right.tsx
+++ src/assets/icons/move-right.tsx
@@ -1,7 +1,19 @@
-const MoveRight = ({ width = 48, height = 48, ...props }) => (
+type MoveRightProps = {
+  width?: number;
+  height?: number;
+  className?: string;
+};
+
+const MoveRight = ({
+  width = 48,
+  height = 48,
+  className,
+  ...props
+}: MoveRightProps) => (
   <svg
     width={width}
     height={height}
+    className={className}
     viewBox="0 0 48 48"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
