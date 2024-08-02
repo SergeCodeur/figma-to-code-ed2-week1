a73db6109b3ee856d1602f779d1b984338f1566d
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-02 01:54:18 +0100

    feat : création de la section collections

--- src/components/sections/Collections.tsx
+++ src/components/sections/Collections.tsx
@@ -1,5 +1,105 @@
+import cyberpunk from "@/assets/images/cyber-punk.png";
+import durolost_boll from "@/assets/images/durolost-boll-upper.png";
+import snoop_dog from "@/assets/images/snoop-dog.png";
+import space_x_wiper from "@/assets/images/space-x-wiper.png";
+import { ReactNode } from "react";
+import { NFTCard, NFTCardProps } from "../ui/card";
+
+type collectionProps = {
+  label: ReactNode;
+  active?: boolean;
+};
+
+// Collection Filter
+const CollectionFilter: collectionProps[] = [
+  {
+    label: "All categories",
+    active: true,
+  },
+  {
+    label: "Art",
+  },
+  {
+    label: "Celebrities",
+  },
+  {
+    label: "Gaming",
+  },
+  {
+    label: "Sport",
+  },
+];
+
+// Collection Buttons Filter
+const CollectionBtnFilter = () => {
+  return (
+    <div className="space-x-4 overflow-x-auto whitespace-nowrap">
+      {CollectionFilter.map((item, index) => (
+        <button
+          key={index}
+          className={`${
+            item.active
+              ? "bg-neutral-800 text-white"
+              : "text-neutral-800 bg-neutral-50"
+          } px-[26px] py-2.5 rounded-xl text-sm font-semibold`}
+        >
+          <span>{item.label}</span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+// NFT Card Content
+const NFTCardData: NFTCardProps[] = [
+  {
+    image: cyberpunk,
+    title: "Cyber Punk",
+    price: 68,
+  },
+  {
+    image: durolost_boll,
+    title: "Durolost  Boll - Upper",
+    price: 68,
+  },
+  {
+    image: space_x_wiper,
+    title: "Space X Wiper",
+    price: 68,
+  },
+  {
+    image: snoop_dog,
+    title: "Snoop Dog",
+    price: 68,
+  },
+];
+
+// Nft Collection Card
+const NFTCollectionCard = () => {
+  return (
+    <>
+      {NFTCardData.map((item, index) => (
+        <NFTCard key={index} {...item} />
+      ))}
+    </>
+  );
+};
+
 const Collections = () => {
-  return <div>Collections</div>;
+  return (
+    <section className="container space-y-9 pb-[100px] pt-16">
+      <h2 className="text-3xl font-bold">Collections</h2>
+      <div className="flex justify-between flex-wrap gap-2.5">
+        <CollectionBtnFilter />
+        <a href="#" className="underline text-sm font-semibold">
+          View more
+        </a>
+      </div>
+      <div className="flex flex-wrap gap-6 justify-center">
+        <NFTCollectionCard />
+      </div>
+    </section>
+  );
 };
 
 export default Collections;
