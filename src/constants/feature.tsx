e59cc8492a270f03bb3b3f3d0bfd023d9117c5b3
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:40:37 +0100

    feat(features): Add feature data for display

---
+++ src/constants/feature.tsx
@@ -0,0 +1,29 @@
+import { Bitcoin, CloudDownload, SaleTag, Wallet } from "@/assets/icons";
+import { FeaturesProps } from "@/types";
+
+export const featuresData: FeaturesProps[] = [
+  {
+    icon: <Wallet />,
+    title: "Set up your wallet",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    icon: <CloudDownload />,
+    title: "Add your NFT's",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    icon: <SaleTag />,
+    title: "Promote your NFT's",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    icon: <Bitcoin />,
+    title: "Sell your NFT's",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+];
