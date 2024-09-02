3f2917b90b41562af50ee16740b8f9549b3238db
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:36:11 +0100

    feat(ui): Create FooterLinks  constants

---
+++ src/constants/footerLinks.ts
@@ -0,0 +1,16 @@
+import { FooterLinksProps } from "@/types";
+
+export const footerLinks: FooterLinksProps[] = [
+  {
+    label: "Privacy",
+    href: "/",
+  },
+  {
+    label: "Terms & Conditions",
+    href: "/",
+  },
+  {
+    label: "About Us",
+    href: "/",
+  },
+];
