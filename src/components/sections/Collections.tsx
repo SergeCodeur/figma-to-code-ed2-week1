5a9a1455fd48214abf83c62b6638d1cdeff62534
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:45:52 +0100

    refactor(ui): Refactor Collections component with reusable components

--- src/components/sections/Collections.tsx
+++ src/components/sections/Collections.tsx
@@ -1,93 +1,5 @@
-import cyberpunk from "@/assets/images/cyber-punk.png";
-import durolost_boll from "@/assets/images/durolost-boll-upper.png";
-import snoop_dog from "@/assets/images/snoop-dog.png";
-import space_x_wiper from "@/assets/images/space-x-wiper.png";
-import { ReactNode } from "react";
-import { NFTCard, NFTCardProps } from "../ui/card";
-
-type collectionProps = {
-  label: ReactNode;
-  active?: boolean;
-};
-
-// Collection Filter
-const CollectionFilter: collectionProps[] = [
-  {
-    label: "All categories",
-    active: true,
-  },
-  {
-    label: "Art",
-    active: false,
-  },
-  {
-    label: "Celebrities",
-    active: false,
-  },
-  {
-    label: "Gaming",
-    active: false,
-  },
-  {
-    label: "Sport",
-    active: false,
-  },
-];
-
-// Collection Buttons Filter
-const CollectionBtnFilter = () => {
-  return (
-    <div className="space-x-4 overflow-x-auto whitespace-nowrap">
-      {CollectionFilter.map((item, index) => (
-        <button
-          key={index}
-          className={`${
-            item.active === true
-              ? "bg-neutral-800 text-white hover:bg-neutral-800/90"
-              : "text-neutral-800 bg-neutral-50 hover:bg-neutral-800/10"
-          } px-[26px] py-2.5 rounded-xl text-sm font-semibold`}
-        >
-          <span>{item.label}</span>
-        </button>
-      ))}
-    </div>
-  );
-};
-
-// NFT Card Content
-const NFTCardData: NFTCardProps[] = [
-  {
-    image: cyberpunk,
-    title: "Cyber Punk",
-    price: 68,
-  },
-  {
-    image: durolost_boll,
-    title: "Durolost  Boll - Upper",
-    price: 68,
-  },
-  {
-    image: space_x_wiper,
-    title: "Space X Wiper",
-    price: 68,
-  },
-  {
-    image: snoop_dog,
-    title: "Snoop Dog",
-    price: 68,
-  },
-];
-
-// Nft Collection Card
-const NFTCollectionCard = () => {
-  return (
-    <>
-      {NFTCardData.map((item, index) => (
-        <NFTCard key={index} {...item} />
-      ))}
-    </>
-  );
-};
+import CollectionBtnFilter from "../CollectionsBtnFilter";
+import NFTCollectionContainer from "../NftCollectionsContainer";
 
 const Collections = () => {
   return (
@@ -100,7 +12,7 @@ const Collections = () => {
         </a>
       </div>
       <div className="flex flex-wrap gap-6 justify-center">
-        <NFTCollectionCard />
+        <NFTCollectionContainer />
       </div>
     </section>
   );
