b653d4f3b1643e265e52c07d3c50a431730c0758
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:39:36 +0100

    feat(ui): Create NFTCardData array with statics data

---
+++ src/constants/nftCollectionsCard.ts
@@ -0,0 +1,28 @@
+import cyberpunk from "@/assets/images/cyber-punk.png";
+import durolost_boll from "@/assets/images/durolost-boll-upper.png";
+import snoop_dog from "@/assets/images/snoop-dog.png";
+import space_x_wiper from "@/assets/images/space-x-wiper.png";
+import { NFTCardProps } from "@/types";
+
+export const NFTCardData: NFTCardProps[] = [
+  {
+    image: cyberpunk,
+    title: "Cyber Punk",
+    price: 68,
+  },
+  {
+    image: durolost_boll,
+    title: "Durolost  Boll - Upper",
+    price: 68,
+  },
+  {
+    image: space_x_wiper,
+    title: "Space X Wiper",
+    price: 68,
+  },
+  {
+    image: snoop_dog,
+    title: "Snoop Dog",
+    price: 68,
+  },
+];
