b7eb1fa2b9d7a2c64dd26df806d5c3323e60af16
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:35:19 +0100

    feat(ui): Create CollectionFilter constants

---
+++ src/constants/collections.ts
@@ -0,0 +1,24 @@
+import { CollectionProps } from "@/types";
+
+export const CollectionFilter: CollectionProps[] = [
+  {
+    label: "All categories",
+    active: true,
+  },
+  {
+    label: "Art",
+    active: false,
+  },
+  {
+    label: "Celebrities",
+    active: false,
+  },
+  {
+    label: "Gaming",
+    active: false,
+  },
+  {
+    label: "Sport",
+    active: false,
+  },
+];
