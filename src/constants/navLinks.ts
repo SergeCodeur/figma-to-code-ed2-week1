224d802662cab2754413c29dc3dc1dbb0b96e7a5
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-03 00:38:21 +0100

    feat(ui): Create Navlinks  constants

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,24 @@
+import { NavLink } from "@/types";
+
+export const Navlinks: NavLink[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Top Sales",
+    href: "#",
+  },
+  {
+    label: "Collections",
+    href: "#",
+  },
+  {
+    label: "Our blog",
+    href: "#",
+  },
+  {
+    label: "About us",
+    href: "#",
+  },
+];
